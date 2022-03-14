8ae784eeea306544db6f2aa789e1b19d3589b83d
paolofullone <paolo.fullone@gmail.com>
2022-03-14 14:05:35 -0300

    fix localStorage monitoria summer

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,8 +1,9 @@
 const PLAYER = 'player';
 
-if (!JSON.parse(localStorage.getItem(PLAYER))) {
-  localStorage.setItem(PLAYER, JSON.stringify([]));
-}
+// Neste caso não precisa pois estamos setando um novo jogador toda vez e não estou atualizando.
+// if (!JSON.parse(localStorage.getItem(PLAYER))) {
+//   localStorage.setItem(PLAYER, JSON.stringify([]));
+// }
 
 const readPlayer = () => JSON.parse(localStorage.getItem(PLAYER));
 // console.log(readPlayer());
@@ -20,9 +21,10 @@ const addLocalStoragePlayer = (user) => {
 // para salvar vários (ranking)
 const PLAYERS_RANKING = 'players_ranking';
 
-if (!JSON.parse(localStorage.getItem(PLAYERS_RANKING))) {
-  localStorage.setItem(PLAYERS_RANKING, JSON.stringify([]));
-}
+// Adicionei o || [] na linha 37 e eliminou a necessidade desse código.
+// if (!JSON.parse(localStorage.getItem(PLAYERS_RANKING))) {
+//   localStorage.setItem(PLAYERS_RANKING, JSON.stringify([]));
+// }
 
 const readPlayers = () => JSON.parse(localStorage.getItem(PLAYERS_RANKING));
 // console.log(readUsers());
