df80b208884886499c38e896d3fbba86f936fd6e
paolofullone <paolo.fullone@gmail.com>
2022-03-14 19:02:40 -0300

    req 17 - botao go home

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
+  handleClickHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleClickHome }
+        >
+          Home
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
