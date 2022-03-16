9b3083131893c3e1d59d4e8ff96f866110b25519
paolofullone <paolo.fullone@gmail.com>
2022-03-16 18:59:52 -0300

    estilizacao em andamento

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Card from '../components/Card';
+import getCategories from '../services/GetCategories';
 
 export default class Config extends Component {
+  state = {
+    categories: null,
+  }
+
+  componentDidMount = async () => {
+    const categories = await getCategories();
+    console.log(categories);
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
-      <h1 data-testid="settings-title">Config</h1>
+      <div>
+        <h1 data-testid="settings-title">Config</h1>
+        <Card />
+      </div>
     );
   }
 }
